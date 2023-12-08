808419add43dca05838245a49efbe7591d29ed83
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-08 16:43:06 -0600

    update Operadores/op_comparacion.py

---
+++ Operadores/op_comparacion.py
@@ -0,0 +1,19 @@
+igual_que = 5 == 6
+
+distinto_que = 5 != 6
+
+mayor_que = 5 > 6
+
+mayor_igual_que = 5 >= 6
+
+menor_que = 5 < 6
+
+menor_igual_que = 5 <= 6
+
+# Calculos combinados
+a = 20
+b = 7
+c = 32
+
+resultado = a + b <= c
+print(resultado)
