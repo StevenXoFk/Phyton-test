730b9194d8f975a18b725a230d6d737a9f1e662b
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-08 17:14:02 -0600

    update condicionales/elif.py

---
+++ condicionales/elif.py
@@ -0,0 +1,11 @@
+plata = 70000
+gasto = 30000
+
+# If anidado y else if (elif)
+
+if plata > 20000:
+  if plata - gasto < 10000 :
+  print("Mucho gasto")
+  elif plata < 20000:
+    print("pobreza")
+    
