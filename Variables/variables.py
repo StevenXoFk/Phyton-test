27a21579303c386b65a66e46233542f4e7012fe1
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-08 16:45:17 -0600

    update Variables/variables.py

---
+++ Variables/variables.py
@@ -0,0 +1,9 @@
+nombre = "Pedro"
+
+# Concatenar con f-strings
+bienvenida = f"Hola {nombre} como estas "
+print(bienvenida)
+
+#Operadores de pertenencia (in y not in)
+print("Pedro" in nombre)
+print("pablo" not in nombre)
