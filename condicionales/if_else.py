0127487661f94c15a1202eb8605e77734d2e2bb9
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-08 16:55:17 -0600

    update condicionales/if_else.py

--- condicionales/if_else.py
+++ condicionales/if_else.py
@@ -1,6 +1,10 @@
-edad = 19
+edad = 16
 
 if edad >= 18:
   print("Pasa")
+  print("Forma Parte del If")
 else: 
   print("No pasa")
+  print("Forma parte del else")
+
+print("No forma parte de ninguna condición ")
