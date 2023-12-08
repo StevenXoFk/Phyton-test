da206d774224f2576401f8eaf291aa4560c411b9
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-08 16:42:24 -0600

    update Operadores/op_aritmeticos.py

---
+++ Operadores/op_aritmeticos.py
@@ -0,0 +1,20 @@
+# Suma y resta ( + y - )
+suma = 12 + 5
+resta = 12 - 5
+
+# Multiplicación y división ( * y / )
+multi = 12 * 5
+division = 12 / 5 # Devuelve un dato float o sea con fracción 
+
+# Potenciación (exponente) (**)
+exponente = 12 ** 5
+
+# Division baja (//)
+division_baja = 12 // 5 # Devuelve entero redondeado hacia abajo
+
+# resto modulo (%)
+modulo = 12 % 5
+
+# type(dato) me devuelve que tipo de dato resto
+tipo_dato = type(division)
+print(tipo_dato)
