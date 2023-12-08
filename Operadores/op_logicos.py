dfca72aba0deaa4e19d164c6c81f549f329b9695
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-08 17:41:43 -0600

    update Operadores/op_logicos.py

---
+++ Operadores/op_logicos.py
@@ -0,0 +1,14 @@
+# and
+r1 = True & True # True.
+r2 = True & False # False
+r3 = 1 & 1 # True
+r4 = 2 & 1 # False
+# or
+r5 = True | False # True
+r6 = False | False # True 
+r7 = False | False # False
+# not
+r8 = not False #True
+r9 = not True # False
+
+print(r8)
