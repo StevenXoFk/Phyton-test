8c2b8441842009471f124c06c4e2adf4e4d87f30
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-12 01:31:38 -0600

    update Repaso/main.py

--- Repaso/main.py
+++ Repaso/main.py
@@ -6,7 +6,6 @@ if altura >= 180:
   print("Eres alto")
 elif altura <= 180:
   print("Eres bajo ")
-"""
 
 def funcionaltura():
   altura = int(input("Coloca tu altura: "))
@@ -15,3 +14,11 @@ def funcionaltura():
     print("eres alto")
   else:
     print("eres bajo")
+    
+"""
+
+horas = 10
+minutos = 9
+segundos = 0
+
+print(f'{horas:0>2}:{minutos:0>2}:{segundos:0>2}')
