9777adb1b0b59849c69f73751ea6331891a9c3e5
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-12 17:45:38 -0600

    update metodos/metodos_cadena.py

--- metodos/metodos_cadena.py
+++ metodos/metodos_cadena.py
@@ -1,10 +1,14 @@
 cadena1 = "HOLAAAA1"
 cadena2 = "holaaaaaa2"
 
+# Convierte el upper() todo el texto en mayuscula
 mayus = cadena2.upper()
+
+# Convierte el lower() todo el texto en minuscula
 minus = cadena1.lower()
 
-# Convierte en minuscula
+# Convierte el capitalize() en la primera letra en mayuscula
+first_mayus = cadena2.capitalize()
 
 
-print(mayus)
+print(first_mayus)
