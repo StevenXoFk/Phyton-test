9531f209ac0c32c2bee45920a15e0602beb517f2
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-11 21:58:19 -0600

    update Repaso/main.py

---
+++ Repaso/main.py
@@ -0,0 +1,17 @@
+# El input guarda el dato escrito en consola para sustituir el valor lmao
+"""
+altura = int(input("Coloca tu altura: "))
+
+if altura >= 180:
+  print("Eres alto")
+elif altura <= 180:
+  print("Eres bajo ")
+"""
+
+def funcionaltura():
+  altura = int(input("Coloca tu altura: "))
+  
+  if altura >= 180:
+    print("eres alto")
+  else:
+    print("eres bajo")
