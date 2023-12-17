9a91010bf83861b22e79958cd6bccf7d19a2691e
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-17 17:01:54 -0600

    update metodos/metodos_listas.py

--- metodos/metodos_listas.py
+++ metodos/metodos_listas.py
@@ -1,27 +1,27 @@
 # Creando una lista con list()
-lista = list(["Steven", "Pepe", 10, 1.2])
+lista = list([73636, 333, 10, 1.2])
 
 # Devuelve la cantidad de elementos en la lista
 cantidad = len(lista)
 
 # Agrega un elemento a la lista
-lista.append("LOOOOOOL")
+lista.append(909)
 
 # Agrega un elemento a la lista en un indice/posición especifica
-lista.insert(1, "Didididi")
+lista.insert(1, 100)
 
 # Agrega varios elementos a la lista
-lista.extend([True, False, 20202, "meme", "pepeepep"])
+lista.extend([True, False, 20202, 202022, 2.555])
 
 # Elimina un elemento de la lista por su indice/posición
 lista.pop(-1) # -1 para eliminar el ultimo, -2 el ante ultimo y asi sucesivamente 
 
 # Remueve un elemento de la lista por su valor
-lista.remove("Steven")
-
-
+lista.remove(99)
 
 #Elimina todos los elementos de la lista
 # lista.clear()
 
+# Ordena la lista ( si se usa el parametro reverse=True se ordena de mayor a menor)
+lista.sort
 print(lista)
