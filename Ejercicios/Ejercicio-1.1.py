1ac0eb52c721d2f24492188dda3c849f1ed3a800
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-17 21:55:06 -0600

    update Ejercicios/Ejercicio-1.1.py

--- Ejercicios/Ejercicio-1.1.py
+++ Ejercicios/Ejercicio-1.1.py
@@ -6,5 +6,9 @@ dalto_curso = 1.5
 
 # Diferencia de duraxion
 diferencia_con_min = 100 - dalto_curso / otros_cursos_min * 100
+diferencia_con_max = 100 - dalto_curso / otros_cursos_max * 100
+diferencia_con_promedio = 100 - dalto_curso / otros_cursos_promedio * 100
 
 print(f'El curso de dalto dura un {diferencia_con_min}% menos que el mas rapido')
+print(f'El curso de dalto dura un {diferencia_con_max}% menos que el mas rapido')
+print(f'El curso de dalto dura un {diferencia_con_promedio}% menos que el mas rapido')
