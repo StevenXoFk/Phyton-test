2422af8040ca39b7786f8ba09d92ba027fb51b7b
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-17 21:11:32 -0600

    update Inputs/Inputs_numeros.py

--- Inputs/Inputs_numeros.py
+++ Inputs/Inputs_numeros.py
@@ -4,4 +4,7 @@ numero = input("Dame un número: ")
 # Convertir el string a int o sea convertir el texto de número a numero
 resultado = int(numero) * 2
 
+# Convertir el string a float o sea convertir el texto de número a numero flotante
+resultado = int(numero) * 2
+
 print(resultado)
