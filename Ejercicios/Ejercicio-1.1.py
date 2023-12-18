642535baeb2c24537d88bd481516a045d7cc701f
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-17 22:07:53 -0600

    update Ejercicios/Ejercicio-1.1.py

--- Ejercicios/Ejercicio-1.1.py
+++ Ejercicios/Ejercicio-1.1.py
@@ -4,11 +4,19 @@ otros_cursos_max = 7
 otros_cursos_promedio = 4
 dalto_curso = 1.5
 
-# Diferencia de duraxion
+# duración crudo
+crudo_promedio = 5
+crudos_dalto = 3.5
+
+# Diferencia de duraxion (ejercicio A)
 diferencia_con_min = 100 - dalto_curso / otros_cursos_min * 100
 diferencia_con_max = 100 - dalto_curso * 1000 // otros_cursos_max / 10
 diferencia_con_promedio = 100 - dalto_curso / otros_cursos_promedio * 100
-
+#Ejercicio A
 print(f'El curso de dalto dura un {diferencia_con_min}% menos que el mas rapido')
 print(f'El curso de dalto dura un {diferencia_con_max}% menos que el mas lento')
 print(f'El curso de dalto dura un {diferencia_con_promedio}% menos que el mas promedio')
+
+#Ejercicio b
+ # calculando el procentaje vacio
+ vacio_promedio = 100 - crudo_promedio / otros_cursos_promedio * 100
