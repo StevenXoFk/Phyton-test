f6a9b6cb190dd7ba1bef19a382daab42f06f59f0
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-17 20:04:28 -0600

    update metodos/metodos_dicts.py

--- metodos/metodos_dicts.py
+++ metodos/metodos_dicts.py
@@ -3,3 +3,8 @@ diccionario = {
   "apellido": "Sech",
   "years": 99
 }
+
+# claves = diccionario.keys() # Me dice que items hay en el diccionario
+claves = diccionario.get("nombre") # Me devuelve el item que mencioné 
+
+print(claves)
