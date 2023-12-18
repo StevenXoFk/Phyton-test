483f16dad623ccfc95036ea3e03261e1ec1787d3
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-17 21:10:41 -0600

    update Inputs/Inputs_numeros.py

---
+++ Inputs/Inputs_numeros.py
@@ -0,0 +1,7 @@
+# Pedirle un numero al usuario
+numero = input("Dame un número: ")
+
+# Convertir el string a int o sea convertir el texto de número a numero
+resultado = int(numero)*2
+
+print(resultado)
