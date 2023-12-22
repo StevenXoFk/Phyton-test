113c2f519e8c0276227e7a8e88bb08b30b5d0aad
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-21 22:16:46 -0600

    update Variables 2.0/Desempaquetado.py

---
+++ Variables 2.0/Desempaquetado.py
@@ -0,0 +1,9 @@
+# Creando datos
+dato_tupla = ("pepe", "sus", 10)
+dato_lista = ["epe", "uss", 100]
+
+#Desempaquetado
+nombre, apellido, numero = dato_tupla
+nombre1, apellido2, numero3 = dato_lista
+
+print(numero)
