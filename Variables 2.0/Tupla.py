94dd300cdc0db308427ab30138825212bccb4c0e
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2023-12-21 22:24:24 -0600

    update Variables 2.0/Tupla.py

---
+++ Variables 2.0/Tupla.py
@@ -0,0 +1,11 @@
+# Creando tupla con tuple()
+tupla = tuple(["pe1", "pe2", "pe3"])
+
+# Creando tupla sin ()
+tupla = "pepe1", "pepe2"
+
+# Creando tupla de un solo dato
+tupla = "tuplaaa",
+
+print(tupla)
+print(type(tupla))
