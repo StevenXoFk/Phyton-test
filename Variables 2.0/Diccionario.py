ba7c2d704410ef90c3ac49092bcd2639e6b34184
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2024-01-01 18:40:08 -0600

    update Variables 2.0/Diccionario.py

--- Variables 2.0/Diccionario.py
+++ Variables 2.0/Diccionario.py
@@ -4,7 +4,10 @@ diccionario = dict(nombre="Pepe",apellido="sech")
 #Las listas no pueden ser claves y usamos frozenset para meter conjuntos
 diccionario2 = {frozenset(["dato", "date"]): "lol"}
 
-#Creando diccionarios con fromkeys() con valor none
+#Creando diccionarios con fromkeys() con valor por defecto: none
 diccionario3 = dict.fromkeys(["nombre","numero"])
 
-print(diccionario3)
+#Creando diccionarios con fromkeys() con 2 parámetros 
+diccionario4 = dict.fromkeys(["nombre","numero"], 'lmao')
+
+print(diccionario4)
