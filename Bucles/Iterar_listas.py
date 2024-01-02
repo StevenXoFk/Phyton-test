3a5ba7294489364652463bf896bc53d561cb7663
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2024-01-02 14:27:09 -0600

    update Bucles/Iterar_listas.py

--- Bucles/Iterar_listas.py
+++ Bucles/Iterar_listas.py
@@ -1,3 +1,4 @@
+#===================================================== TODO LO SIGUIENTE TAMBIEN FUNCIONA CON TUPLAS Y                          LISTAS       =====================================================
 animales = ["perro","gato","loro","mono","pez"]
 numeros = [1,90,62,77,82]
 orden = 0
