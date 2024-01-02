d416541c9fb671acdf1812e1ca9150d8862bd565
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2024-01-01 18:13:07 -0600

    update Variables 2.0/Conjuntos.py

--- Variables 2.0/Conjuntos.py
+++ Variables 2.0/Conjuntos.py
@@ -5,4 +5,12 @@ conjunto = set(["dato1", "dato2"])
 conjunto1 = frozenset(["dato33", "dato34"])
 conjunto2 = {conjunto1, "asd"}
 
-print(conjunto2)
+#Teoria de conjuntos
+conjunto3 = {1,3,9,5,4}
+conjunto4 = {1,3,4}
+
+# Verificar si es un subconjunto
+resultado = conjunto4.issubset(conjunto3) #conjunto4 es un subconjunto de conjunto3
+resultado2 = conjunto4 <= conjunto3 # Otra forma de Verificar los subconjuntos
+
+print(resultado2)
