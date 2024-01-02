85a2a60c28828dd57fcdcdf44d5c1ef72f1e1f6b
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2024-01-01 18:02:39 -0600

    update Variables 2.0/Conjuntos.py

--- Variables 2.0/Conjuntos.py
+++ Variables 2.0/Conjuntos.py
@@ -1,4 +1,7 @@
 # Creando un conjunto con set()
 conjunto = set(["dato1", "dato2"])
 
-print(conjunto)
+#metiendo un conjunto en otro conjunto
+conjunto1 = frozenset(["dato33", "dato34"])
+conjunto2 = {conjunto1, "asd"}
+print(conjunto2)
