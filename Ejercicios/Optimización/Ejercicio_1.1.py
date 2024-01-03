cf50867d6f666c2ef683029fa728839adb6d71af
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2024-01-02 19:20:13 -0600

    update Ejercicios/Optimización/Ejercicio_1.1.py

---
+++ Ejercicios/Optimización/Ejercicio_1.1.py
@@ -0,0 +1,21 @@
+# Promedio de duración
+otros_cursos_min = 2.5
+otros_cursos_max = 7
+otros_cursos_promedio = 4
+dalto_curso = 1.5
+
+# duración crudo
+crudo_promedio = 5
+crudos_dalto = 3.5
+
+# Diferencia de duraxion (ejercicio A)
+diferencia_con_min = round(100 - dalto_curso / otros_cursos_min * 100)
+diferencia_con_max = round(100 - dalto_curso / otros_cursos_max * 100,1)
+diferencia_con_promedio = round(100 - dalto_curso / otros_cursos_promedio * 100,1)
+
+#Ejercicio A
+print("-----------------")
+print(f'El curso de dalto dura un {diferencia_con_min}% menos que el mas rapido')
+print(f'El curso de dalto dura un {diferencia_con_max}% menos que el mas lento')
+print(f'El curso de dalto dura un {diferencia_con_promedio}% menos que el mas promedio')
+print("-----------------")
