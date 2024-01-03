bab62b2e6dadad3237099dca90e4bd9fbca56618
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2024-01-02 22:18:23 -0600

    update Repaso/Fix/Incio_sesion.py

--- Repaso/Fix/Incio_sesion.py
+++ Repaso/Fix/Incio_sesion.py
@@ -4,21 +4,20 @@ nuestro sistema, recuerda que tu nombre de usuario
 debe de tener al menos mas de 4 caracteres y maximo
 de 20
 ---------------------------------------------------------------""")
+error = False
 usuario = input("Registrata tu nombre de usuario maximo 20 caracteres: ")
 contraseña = input("Pon tu contraseña")
 
+while error == True:
+  print("JSHSHSHSHSHJA")
 
 # Funciones
-def registro_fail():
-  if len(usuario) > 20:
-  print("Tu nombre de usuario exede los 20 caracteres, vuelve a intentarlo")
-  
-elif len(usuario) <= 3 or len(usuario) <= 1:
-  print("Tu nombre de usuario tiene menos de 4 caracteres, intentalo de nuevo")
-  
-elif len(contraseña) < 5 or len(contraseña) <= 1:
-  print("Tu contraseña es muy corta, intenta de nuevo")
-  
 def volver_registro():
   usuario = input("Registrata tu nombre de usuario: ")
   contraseña = input("Pon tu contraseña")
+
+def registro_fail():
+  if len(usuario) > 20:
+    print("Tu nombre de usuario tiene menos de 4 caracteres, intentalo de nuevo")
+    error = True
+    volver_registro()
