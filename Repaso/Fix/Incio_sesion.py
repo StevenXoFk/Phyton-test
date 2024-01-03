ac11a010bdc941198099f2aaced58b2d741b8da5
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2024-01-02 23:54:18 -0600

    update Repaso/Fix/Incio_sesion.py

--- Repaso/Fix/Incio_sesion.py
+++ Repaso/Fix/Incio_sesion.py
@@ -5,18 +5,21 @@ def volver_registro():
 
 def registro_fail():
   global usuario, contraseña
- 
+  
   while len(usuario) > 20:
     print("Tu nombre de usuario tiene mas de 20 caracteres, intentalo de nuevo")
     usuario, contraseña = volver_registro()
+    break
     
   while len(usuario) <= 3 or len(usuario) <= 1:
     print("Tu nombre de usuario tiene menos de 4 caracteres, intentalo de nuevo")
     usuario, contraseña = volver_registro()
+    break
   
   while len(contraseña) < 5 or len(contraseña) <= 1:
     print("Tu contraseña es muy corta, intenta de nuevo")
     usuario, contraseña = volver_registro()
+    break
 def inicio_fail():
   print("Hola we")
   
@@ -28,4 +31,3 @@ de 20
 ---------------------------------------------------------------""")
 usuario = input("Registrata tu nombre de usuario maximo 20 caracteres: ")
 contraseña = input("Pon tu contraseña")
-comprobar_registro = True
