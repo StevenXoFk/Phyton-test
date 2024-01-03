bba0efd4b04eaa6706a6e95f045fb1bc068da035
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2024-01-03 14:23:51 -0600

    update Funciones/Funciones_lambda.py

--- Funciones/Funciones_lambda.py
+++ Funciones/Funciones_lambda.py
@@ -1,4 +1,9 @@
+numeros = [1,2,8,4,6]
 
 #Creando una funcion lambda para multiplicar por 2
 multiplicar_por_dos = lambda x : x*2
 print(multiplicar_por_dos(4))
+
+#Lo mismo pero con lambda filter
+numeros_pares = filter(lambda numero:numero%2 == 0, numeros)
+print(list(numeros_pares))
