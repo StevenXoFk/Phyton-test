bad5a5d6fbeb31b0092964aff08a3f14292ccd59
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2024-01-02 22:54:53 -0600

    update Repaso/Fix/Incio_sesion.py

--- Repaso/Fix/Incio_sesion.py
+++ Repaso/Fix/Incio_sesion.py
@@ -1,3 +1,21 @@
+# Funciones
+def volver_registro():
+  usuario = input("Registrata tu nombre de usuario: ")
+  contraseña = input("Pon tu contraseña")
+
+def registro_fail():
+  if len(usuario) > 20:
+    print("Tu nombre de usuario tiene mas de 20 caracteres, intentalo de nuevo")
+    volver_registro()
+    
+  elif len(usuario) <= 3 or len(usuario) <= 1:
+    print("Tu nombre de usuario tiene menos de 4 caracteres, intentalo de nuevo")
+    volver_registro()
+  
+  elif len(contraseña) < 5 or len(contraseña) <= 1:
+    print("Tu contraseña es muy corta, intenta de nuevo")
+    volver_registro()
+    
 print("""---------------------------------------------------------------
 Bienvenido a PichaWeb, registrate para acceder a
 nuestro sistema, recuerda que tu nombre de usuario
@@ -6,6 +24,8 @@ de 20
 ---------------------------------------------------------------""")
 usuario = input("Registrata tu nombre de usuario maximo 20 caracteres: ")
 contraseña = input("Pon tu contraseña")
-volver_registro()
+comprobar = True
 
-
+while comprobar == True:
+  registro_fail()
+  
