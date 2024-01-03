757b6b5708c66a09de217bcf3cd28ee64892e5a2
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2024-01-02 19:56:34 -0600

    update Funciones/Build_in.py

--- Funciones/Build_in.py
+++ Funciones/Build_in.py
@@ -11,3 +11,15 @@ print(num_bajo)
 # Redondear a 6 decimales
 num_round = round(num,2)
 print(num_round)
+
+# Retorna false -> vacio, false, None \ True -> distinto a 0, textos, datos no vacios
+resultado_bool = bool(1)
+print(resultado_bool)
+
+# Retorna true si todos los datos son verdaderos
+resultado_all = all([0,True,"hahsgs"])
+print(resultado_all)
+
+# Suma yodos los datos de un iterable (solo números 
+resultado_sum = sum(numero)
+print(resultado_sum)
