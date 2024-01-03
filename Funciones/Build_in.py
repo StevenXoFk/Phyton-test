6459884a1b76d2af235144a128b9bb0e18109e0f
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2024-01-02 19:08:05 -0600

    update Funciones/Build_in.py

--- Funciones/Build_in.py
+++ Funciones/Build_in.py
@@ -1,5 +1,5 @@
 numero = [1,88,72,98,3]
-
+num = 17.8362
 # Encontrar el numero mas alto
 num_alto = max(numero)
 print(num_alto)
@@ -7,3 +7,7 @@ print(num_alto)
 # Encontrar el numero mas bajo
 num_bajo = min(numero)
 print(num_bajo)
+
+# Redondear a 6 decimales
+num_round = round(num,2)
+print(num_round)
