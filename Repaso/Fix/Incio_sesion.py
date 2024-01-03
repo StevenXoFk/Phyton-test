c8d1660fc812554eb8ba095163aee5e49dff08b8
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2024-01-02 23:43:41 -0600

    update Repaso/Fix/Incio_sesion.py

--- Repaso/Fix/Incio_sesion.py
+++ Repaso/Fix/Incio_sesion.py
@@ -4,24 +4,21 @@ def volver_registro():
   contraseña = input("Pon tu contraseña")
 
 def registro_fail():
-  if len(usuario) > 20:
+  global usuario, contraseña
+ 
+  while len(usuario) > 20:
     print("Tu nombre de usuario tiene mas de 20 caracteres, intentalo de nuevo")
-    volver_registro()
+    usuario, contraseña = volver_registro()
     
-  elif len(usuario) <= 3 or len(usuario) <= 1:
+  while len(usuario) <= 3 or len(usuario) <= 1:
     print("Tu nombre de usuario tiene menos de 4 caracteres, intentalo de nuevo")
-    volver_registro()
+    usuario, contraseña = volver_registro()
   
-  elif len(contraseña) < 5 or len(contraseña) <= 1:
+  while len(contraseña) < 5 or len(contraseña) <= 1:
     print("Tu contraseña es muy corta, intenta de nuevo")
-    volver_registro()
-  else:
-    comprobar_inicio = True
-    comprobar_registro = False
-    
+    usuario, contraseña = volver_registro()
 def inicio_fail():
   print("Hola we")
-  
   
 print("""---------------------------------------------------------------
 Bienvenido a PichaWeb, registrate para acceder a
@@ -32,13 +29,3 @@ de 20
 usuario = input("Registrata tu nombre de usuario maximo 20 caracteres: ")
 contraseña = input("Pon tu contraseña")
 comprobar_registro = True
-
-while comprobar_registro == True:
-  registro_fail()
-
-if comprobar_inicio == True:
-  print(f'Bienvenido {usuario}, te has registrado exitosamente, ahora inicia sesion')
-  inicio_usuario = input("nombre de usuario: ")
-  inicio_contraseña = input("contraseña: ")
-  while comprobar_inicio == True:
-    inicio_fail()
