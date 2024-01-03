074dabe4ae5f10b595a8bd384eb420f84baed6b5
StevenXoFk <87964346+StevenXoFk@users.noreply.github.com>
2024-01-03 15:07:05 -0600

    update Ejercicios/Ejercicio-2.1.py

--- Ejercicios/Ejercicio-2.1.py
+++ Ejercicios/Ejercicio-2.1.py
@@ -1,7 +1,16 @@
 def obtener_compañeros(cantidad):
   compañeros = []
-  for i in range(cantidad_compañeros):
+  for i in range(cantidad):
     nombre = input("Cual es tu nombre: ")
     edad = int(input("Cual es tu edad: "))
     compañero = (nombre,edad)
     compañeros.append(compañero)
+  compañeros.sort(key=lambda x:x [1])
+  asistente = compañeros[0][0]
+  profesor = compañeros[-1][0]
+  return asistente,profesor
+  
+asistente,profesor = obtener_compañeros(3)
+
+print(f'El asistente es: {asistente}')
+print(f'El Profesor es: {profesor}')
